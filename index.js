64919595430947aef4f51d4b4af91b1f2b125aa8
Phate334 <phate334@gmail.com>
2015-12-12 19:21:05 +0800

    update replace target
    換行字元會被取代成空白，接著把斷字的連字號跟空白清掉。

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+/* global $ */
 function replaceNewline()
 {
     var data = $("#text").val();
-    var result = data.replace(/\n/g,"");
+    var result = data.replace(/\n/g," ").replace(new RegExp("- ", "g"),"");
     $("#text").val(result);
 }
